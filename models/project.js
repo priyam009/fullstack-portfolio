428947e8633d1afe1d4d748d3318b04560904e4b
Priyam <priyam009@live.com>
2019-10-07 22:09:59 +1100

    Add Projects table

---
+++ models/project.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, Datatypes) {
+  var Projects = sequelize.define("Projects", {
+    project: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    github: {
+      type: Datatypes.STRING,
+      allowNull: false.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    url: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    hashTags: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    //Timestamps
+    timestamps: true,
+    updatedAt: false
+  });
+
+  return Projects;
+};
