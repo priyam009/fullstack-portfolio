9cee70cf8bcf194772212876de2116f35ea838e7
Priyam <priyam009@live.com>
2019-10-11 23:06:49 +1100

    Add auto scroll to location on click and add button data collection function

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  //Jump to section- Home/Portfolio/Contact
+  $(".page-lnk").click(function() {
+    var anchor = $(this).attr("dest");
+
+    $("html, body").animate(
+      {
+        scrollTop: $("#" + anchor).offset().top
+      },
+      1000
+    );
+  });
+
+  //Store selected #hashTags
+  var selectedTag = [];
+
+  //Change state and add to selectedTag array
+  $("button").on("click", function() {
+
+    if($(this).attr("data-status") === "inactive") {
+      $(this).attr("data-status", "active");
+      var tag = $(this).attr("id");
+      selectedTag.push(tag);
+      console.log("selectedTag", selectedTag);
+    } else {
+      $(this).attr("data-status", "inactive");
+      var tag = $(this).attr("id");
+      var index = selectedTag.indexOf(tag);
+      selectedTag.splice(index, 1);
+      console.log("selectedTag", selectedTag);
+    }
+  });
+
+
+
+});
